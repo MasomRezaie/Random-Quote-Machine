e301a44c9246e2a15d8db020d2f6096c325d4217
MasomRezaie <masom.rezaie135@gmail.com>
2024-08-22 12:48:29 -0700

    Install React & HTML file edit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import HomeBody from "./Component/HomeBody";
+import Nav from "./Component/Nav";
+function App() {
+  return (
+    <>
+
+    <Nav/>
+    <div className="App">
+     <h1>Hello</h1>
+    </div>
+    <HomeBody/>
+    </>
+  );
+}
+
+export default App;
